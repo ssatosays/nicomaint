4df96d726314327092fe315bb4f9d45d547149ae
ssatosays <ssatosays@gmail.com>
2022-01-30 22:21:02 +0900

    Add make_response

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 
 from utils.utils import get_latest_articles
 
@@ -7,8 +7,9 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    resp = get_latest_articles()
-    return jsonify(resp), 200, {'Content-type': 'application/json; charset=UTF-8'}
+    resp = make_response(jsonify(get_latest_articles()))
+    resp.headers['Content-Type'] = 'application/json; charset=UTF-8'
+    return resp
 
 
 if __name__ == '__main__':
