a9bce1f3930a01d74cbbc0e3220bf1cb52513c3b
ssatosays <ssatosays@gmail.com>
2022-02-01 19:44:34 +0900

    Add _del_sent_articles()

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,39 @@
+import json
+import os
 import requests
 from bs4 import BeautifulSoup
+
+JSON_PATH = './utils/articles.json'
+
+
+def _read_json() -> list:
+    with open(JSON_PATH, 'r') as f:
+        return json.load(f)
+
+
+def _update_json(target: list):
+    with open(JSON_PATH, 'w') as f:
+        json.dump(target, f, indent=4, ensure_ascii=False)
+
+
+def _del_sent_articles(latest_articles: list):
+    if not os.path.exists(JSON_PATH):
+        _update_json(latest_articles)
+        return
+    la_titles = [l['title'] for l in latest_articles]
+    rj_titles = [r['title'] for r in _read_json()]
+    latest = list(set(la_titles) - set(rj_titles))
+    if len(latest) > 0:
+        _update_json(latest_articles)
+        diff = []
+        for title in latest:
+            for la in latest_articles:
+                if title == la['title']:
+                    diff.append(la)
+        latest_articles.clear()
+        latest_articles.extend(diff)
+    else:
+        latest_articles.clear()
 
 
 def get_latest_articles():
@@ -12,13 +46,14 @@ def get_latest_articles():
 
     latest_articles = []
     links = first_pagination.find_all('a')
-    for i, link in enumerate(links):
+    for i, link in enumerate(links, start=1):
         latest_articles.append({
             'index': i,
             'url': f"{BASE_URL}{link.get('href')}",
             'title': link.find('p', class_='l-main l-main-list2-title').text,
             'date': link.find('p', class_='l-main l-main-list2-date').text
         })
+    _del_sent_articles(latest_articles)
     return latest_articles
 
 
